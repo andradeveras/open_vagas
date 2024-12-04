160d97e7dfdaada2261a7cdc58026e1b5519fc6e
Leonardo Veras <andradeveras96@gmail.com>
2024-12-04 20:34:11 -0300

    Configure timezone and i18n

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,26 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+def set_locate_configs_and_timezone
+  # Definindo o fuso horário
+  config.time_zone = 'Brasilia'
+
+  # Inicializando a configuração load_paths corretamente
+  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**/*.{rb,yml}')]
+
+  # Definindo as localidades disponíveis como símbolos
+  config.i18n.available_locales = [:pt, :en]
+
+  # Definindo a localidade padrão
+  config.i18n.default_locale = :pt
+end
+
 module OpenVagas
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    set_locate_configs_and_timezone
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
